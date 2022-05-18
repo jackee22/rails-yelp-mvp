495c18714dc21d8be1b080c0ba7adb8ff1c1812c
jack hanna <jack_hanna1@hotmail.com>
2022-05-18 14:30:53 +0200

    first setup models and validations

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Restaurant.new(name: 'Epicure', category: 'french', address: '75 rue du avenue, 7000 Paris', phone_number: '0479115350').save
+Restaurant.new(name: 'Kebab', category: 'italian', address: '77 rue haute, 1000 bruxelles', phone_number: '0498143682').save
+Restaurant.new(name: 'People', category: 'belgian', address: '102 avenue emile verhaeren, 1348 LLN', phone_number: '045372811').save
+Restaurant.new(name: 'Le lac', category: 'italian', address: '53 rue du lac, 1300 wavre', phone_number: '0489153629').save
+Restaurant.new(name: 'Rabelais', category: 'japanese', address: '322 chaussée de louvain, 1140 Evere', phone_number: '04618291734').save
